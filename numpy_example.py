746c69e8ea39cc84837722dd391688c58df030fc
xxcqc250 <a0963272507@gmail.com>
2017-05-06 18:20:57 +0800

    update1
    update a file

---
+++ numpy_example.py
@@ -0,0 +1,11 @@
+import numpy as np
+a=np.arange(60).reshape(12,5)
+print(a)
+print("Dimention is:")
+print (a.ndim)
+print("dtype name is:")
+print(a.dtype)
+print("itemsize is:")
+print(a.itemsize)
+print("type is:")
+print(type(a))
